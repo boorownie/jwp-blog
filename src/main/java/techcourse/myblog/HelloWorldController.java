3a8bc5859689e2260b37346dd8407b3c335909e2
butomop <butumop@dotol.net>
2019-07-11 20:14:12 +0900

    [Add] HelloWorldController - get,post "/helloworld" -> index
    [Modify]
    index.html -> using thymeleaf templates

--- src/main/java/techcourse/myblog/HelloWorldController.java
+++ src/main/java/techcourse/myblog/HelloWorldController.java
@@ -1,13 +1,23 @@
 package techcourse.myblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
-    @GetMapping("/")
-    public String index(){ return "index"; }
 
-    @GetMapping("articles/new")
-    public String newArticle(){ return "article-edit"; }
+    @GetMapping("/helloworld")
+    public String showBlogPage(@RequestParam(name = "blogName", required = false, defaultValue = "Welcome Brown!") String name, Model model) {
+        model.addAttribute("blogName", name);
+        return "index";
+    }
+
+    @PostMapping("/helloworld")
+    public String showBlogPage2(@RequestParam(name = "blogName", required = false, defaultValue = "Welcome Brown!") String name, Model model)   {
+        model.addAttribute("blogName",name);
+        return "index";
+    }
 }
--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,10 +2,17 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import techcourse.myblog.domain.ArticleRepository;
 
 @Controller
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+
+    @GetMapping("/")
+    public String index(){ return "index"; }
+
+    @GetMapping("articles/new")
+    public String newArticle(){ return "article-edit"; }
 }

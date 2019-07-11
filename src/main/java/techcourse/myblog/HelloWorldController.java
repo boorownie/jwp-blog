6472cdeaa1860dc27ddd6d1dddaf46ca522922f0
butomop <butumop@dotol.net>
2019-07-11 20:24:17 +0900

    [Modify] Get,PostMapping -> RequestMapping

--- src/main/java/techcourse/myblog/HelloWorldController.java
+++ src/main/java/techcourse/myblog/HelloWorldController.java
@@ -2,21 +2,13 @@ package techcourse.myblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloWorldController {
 
-    @GetMapping("/helloworld")
-    public String showBlogPage(@RequestParam(name = "blogName", required = false, defaultValue = "Welcome Brown!") String name, Model model) {
-        model.addAttribute("blogName", name);
-        return "index";
-    }
-
-    @PostMapping("/helloworld")
-    public String showBlogPage2(@RequestParam(name = "blogName", required = false, defaultValue = "Welcome Brown!") String name, Model model)   {
+    @RequestMapping(value = {"/helloworld","/"}, method = { RequestMethod.GET, RequestMethod.POST })
+    public String showBlogPage(@RequestParam(name = "blogName",required = false, defaultValue = "Welcome Brown!") String name,Model model){
         model.addAttribute("blogName",name);
         return "index";
     }

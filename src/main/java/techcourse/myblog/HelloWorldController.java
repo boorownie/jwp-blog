3ae575ed3d9aac5b88b629cccd18fdaaad39767e
butomop <butumop@dotol.net>
2019-07-11 19:59:02 +0900

    [Start]
    [Add]
    ArticleController
    - get "/" -> index
    - get "articles/new" -> article-edit
    
    templates
    - article-edit.html
    - index.html

--- src/main/java/techcourse/myblog/HelloWorldController.java
+++ src/main/java/techcourse/myblog/HelloWorldController.java
@@ -1,8 +1,13 @@
 package techcourse.myblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloWorldController {
+    @GetMapping("/")
+    public String index(){ return "index"; }
 
+    @GetMapping("articles/new")
+    public String newArticle(){ return "article-edit"; }
 }

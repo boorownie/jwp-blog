50077a265f013672d3d46a8b60acb22ac8948f66
butomop <butumop@dotol.net>
2019-07-14 05:23:33 +0900

    [Add] post "articles" -> article.html

--- src/main/java/techcourse/myblog/web/ArticleController.java
+++ src/main/java/techcourse/myblog/web/ArticleController.java
@@ -2,8 +2,13 @@ package techcourse.myblog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import techcourse.myblog.domain.ArticleRepository;
+
+import java.util.Map;
 
 @Controller
 public class ArticleController {
@@ -13,6 +18,12 @@ public class ArticleController {
     @GetMapping("/")
     public String index(){ return "index"; }
 
-    @GetMapping("articles/new")
+    @GetMapping(value = {"articles/new","writing"})
     public String newArticle(){ return "article-edit"; }
+
+    @PostMapping("articles")
+    public String saveArticle(@RequestParam Map<String, String> requestParams, Model model){
+        model.addAllAttributes(requestParams);
+        return "article";
+    }
 }
